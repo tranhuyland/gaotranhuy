ef73a74725489902bb8222f638c157483771343b
tranhuyland <huy07kt@gmail.com>
2026-06-29 20:50:17 +0700

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function HomePage() {
+  return (
+    <main
+      style={{
+        padding: 40
+      }}
+    >
+      <h1>Gạo Trần Huy</h1>
+
+      <p>Starter V1</p>
+    </main>
+  );
+}
