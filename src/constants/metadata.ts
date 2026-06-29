cba3182e59f018171c9db74536b257959801cc9a
tranhuyland <huy07kt@gmail.com>
2026-06-29 21:54:49 +0700

    Create metadata.ts

---
+++ src/constants/metadata.ts
@@ -0,0 +1,33 @@
+import { siteConfig } from "@/config/site";
+
+export const defaultMetadata = {
+  title: siteConfig.title,
+
+  description: siteConfig.description,
+
+  keywords: siteConfig.keywords,
+
+  openGraph: {
+    title: siteConfig.title,
+
+    description: siteConfig.description,
+
+    url: siteConfig.url,
+
+    siteName: siteConfig.name,
+
+    locale: siteConfig.locale,
+
+    type: "website",
+
+    images: [
+      {
+        url: siteConfig.ogImage,
+
+        width: 1200,
+
+        height: 630
+      }
+    ]
+  }
+};
