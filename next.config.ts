68ffd9f65a8e8d1041560bb1b72e5504f6d93cf6
tranhuyland <huy07kt@gmail.com>
2026-06-29 21:57:52 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  poweredByHeader: false,
+
+  compress: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
