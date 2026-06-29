17bc9ff21396ed802b0cb63b25312ea278029663
tranhuyland <huy07kt@gmail.com>
2026-06-30 01:18:24 +0700

    Create products.ts

---
+++ src/data/products.ts
@@ -0,0 +1,30 @@
+export const featuredProducts = [
+  {
+    id: "1",
+    slug: "gao-st25",
+    name: "Gạo ST25",
+    image: "/images/products/st25.webp",
+    price: "220.000đ / 5kg",
+  },
+  {
+    id: "2",
+    slug: "gao-jasmine",
+    name: "Gạo Jasmine",
+    image: "/images/products/jasmine.webp",
+    price: "180.000đ / 5kg",
+  },
+  {
+    id: "3",
+    slug: "gao-bac-huong",
+    name: "Gạo Bắc Hương",
+    image: "/images/products/bac-huong.webp",
+    price: "195.000đ / 5kg",
+  },
+  {
+    id: "4",
+    slug: "gao-nang-thom",
+    name: "Gạo Nàng Thơm",
+    image: "/images/products/nang-thom.webp",
+    price: "210.000đ / 5kg",
+  },
+];
