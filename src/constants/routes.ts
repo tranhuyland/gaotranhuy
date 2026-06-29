e35641db151e501404370ca0c6b236ed4b4b6342
tranhuyland <huy07kt@gmail.com>
2026-06-29 21:53:43 +0700

    Add ROUTES constant for application navigation

---
+++ src/constants/routes.ts
@@ -0,0 +1,15 @@
+export const ROUTES = {
+  HOME: "/",
+
+  PRODUCTS: "/san-pham",
+
+  NEWS: "/tin-tuc",
+
+  ABOUT: "/gioi-thieu",
+
+  CONTACT: "/lien-he",
+
+  CART: "/gio-hang",
+
+  CHECKOUT: "/dat-hang"
+} as const;
