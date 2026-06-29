338b695ed23fe933e1ca6486a7e3cf4959124983
tranhuyland <huy07kt@gmail.com>
2026-06-29 21:52:26 +0700

    Create navigation.ts

---
+++ src/config/navigation.ts
@@ -0,0 +1,31 @@
+export interface NavigationItem {
+  label: string;
+  href: string;
+}
+
+export const navigation: NavigationItem[] = [
+  {
+    label: "Trang chủ",
+    href: "/"
+  },
+
+  {
+    label: "Sản phẩm",
+    href: "/san-pham"
+  },
+
+  {
+    label: "Tin tức",
+    href: "/tin-tuc"
+  },
+
+  {
+    label: "Giới thiệu",
+    href: "/gioi-thieu"
+  },
+
+  {
+    label: "Liên hệ",
+    href: "/lien-he"
+  }
+];
