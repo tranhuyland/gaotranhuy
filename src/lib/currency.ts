92113c778de5e1040fc1aee67512370304906db9
tranhuyland <huy07kt@gmail.com>
2026-06-29 22:38:07 +0700

    Create currency.ts

---
+++ src/lib/currency.ts
@@ -0,0 +1,7 @@
+export function formatPrice(price: number) {
+  return new Intl.NumberFormat("vi-VN", {
+    style: "currency",
+    currency: "VND",
+    maximumFractionDigits: 0,
+  }).format(price);
+}
