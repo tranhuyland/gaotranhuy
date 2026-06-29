eda89c1a4cce3c99fe03a1ddce82960a21260799
tranhuyland <huy07kt@gmail.com>
2026-06-29 23:28:26 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 import {
   Logo,
   MobileMenu,
+  SearchBox,
 } from "@/components/layout";
 
 export default function HomePage() {
   return (
     <main className="mx-auto max-w-7xl p-6">
+
       <div className="flex items-center justify-between">
+
         <Logo />
 
+        <SearchBox />
+
         <MobileMenu />
+
       </div>
 
       <h2 className="mt-10 text-3xl font-bold">
@@ -17,6 +23,7 @@ export default function HomePage() {
       </h2>
 
       <p>Starter V1.0</p>
+
     </main>
   );
 }
