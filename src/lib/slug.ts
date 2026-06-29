a22a8983a53cf4b2a1c78fabcb42208ce8746bcb
tranhuyland <huy07kt@gmail.com>
2026-06-29 22:38:28 +0700

    Create slug.ts

---
+++ src/lib/slug.ts
@@ -0,0 +1,11 @@
+export function slugify(text: string) {
+  return text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/đ/g, "d")
+    .replace(/Đ/g, "D")
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
