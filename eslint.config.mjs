04800e8d07e6d1140f642bc821802c5b56cf67b8
tranhuyland <huy07kt@gmail.com>
2026-06-29 22:09:55 +0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,5 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 
 export default [
-  ...nextVitals,
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-      "react/no-unescaped-entities": "off"
-    }
-  }
+  ...nextVitals
 ];
