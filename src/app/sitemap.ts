e41d6ac96984e4c64435caf0858a154bcf13bde4
tranhuyland <huy07kt@gmail.com>
2026-06-30 07:33:28 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,27 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://gaotranhuy.vercel.app",
-      lastModified: new Date(),
+      url: "https://gaotranhuy.vn",
       changeFrequency: "daily",
       priority: 1,
+    },
+
+    {
+      url: "https://gaotranhuy.vn/san-pham",
+      changeFrequency: "daily",
+      priority: 0.9,
+    },
+
+    {
+      url: "https://gaotranhuy.vn/gioi-thieu",
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+
+    {
+      url: "https://gaotranhuy.vn/lien-he",
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
   ];
 }
