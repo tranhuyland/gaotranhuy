7c1a630412326af656f573c0123cf59be4139091
tranhuyland <huy07kt@gmail.com>
2026-06-30 07:55:30 +0700

    Update metadata.ts

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,33 +1,25 @@
+import type { Metadata } from "next";
 import { siteConfig } from "@/config/site";
 
-export const defaultMetadata = {
+export const defaultMetadata: Metadata = {
   title: siteConfig.title,
-
   description: siteConfig.description,
-
   keywords: siteConfig.keywords,
 
   openGraph: {
     title: siteConfig.title,
-
     description: siteConfig.description,
-
     url: siteConfig.url,
-
     siteName: siteConfig.name,
-
     locale: siteConfig.locale,
-
     type: "website",
 
     images: [
       {
         url: siteConfig.ogImage,
-
         width: 1200,
-
-        height: 630
-      }
-    ]
-  }
+        height: 630,
+      },
+    ],
+  },
 };
