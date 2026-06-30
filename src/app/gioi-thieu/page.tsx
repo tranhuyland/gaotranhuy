ecad8c626b90a56d51801b91f1ef8ab54807ec6b
tranhuyland <huy07kt@gmail.com>
2026-06-30 07:39:55 +0700

    Create page.tsx

---
+++ src/app/gioi-thieu/page.tsx
@@ -0,0 +1,13 @@
+export default function ProductsPage() {
+  return (
+    <main className="mx-auto max-w-7xl px-4 py-16">
+      <h1 className="text-4xl font-bold">
+        Giới Thiệu
+      </h1>
+
+      <p className="mt-4 text-gray-600">
+         Gạo ngon Trần Huy
+      </p>
+    </main>
+  );
+}
