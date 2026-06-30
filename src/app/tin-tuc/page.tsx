71261b166cce786d7c609e85a064891589df77cb
tranhuyland <huy07kt@gmail.com>
2026-06-30 07:38:53 +0700

    Create page.tsx

---
+++ src/app/tin-tuc/page.tsx
@@ -0,0 +1,13 @@
+export default function ProductsPage() {
+  return (
+    <main className="mx-auto max-w-7xl px-4 py-16">
+      <h1 className="text-4xl font-bold">
+        tin tức
+      </h1>
+
+      <p className="mt-4 text-gray-600">
+        tị tứ mới nhấy sẽ được cập nhật tại đây.
+      </p>
+    </main>
+  );
+}
