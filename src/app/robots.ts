e51121b555c1f2ce6c532a2c3f35abf7696a1c6a
tranhuyland <huy07kt@gmail.com>
2026-06-30 07:33:02 +0700

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,12 +2,11 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-      },
-    ],
-    sitemap: "https://gaotranhuy.vercel.app/sitemap.xml",
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+
+    sitemap: "https://gaotranhuy.vn/sitemap.xml",
   };
 }
