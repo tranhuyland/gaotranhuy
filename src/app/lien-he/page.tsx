a1f393ccb9df4cd29e02359134698a79fc9c18ee
tranhuyland <huy07kt@gmail.com>
2026-06-30 07:40:37 +0700

    Create page.tsx

---
+++ src/app/lien-he/page.tsx
@@ -0,0 +1,13 @@
+export default function ProductsPage() {
+  return (
+    <main className="mx-auto max-w-7xl px-4 py-16">
+      <h1 className="text-4xl font-bold">
+        Liên Hệ
+      </h1>
+
+      <p className="mt-4 text-gray-600">
+        0931555551
+      </p>
+    </main>
+  );
+}
