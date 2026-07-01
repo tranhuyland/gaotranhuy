ea79e790bc89f26be6beb6e4445e95f566d70544
tranhuyland <huy07kt@gmail.com>
2026-07-01 10:11:42 +0700

    Add export for AddToCartButton component

--- src/features/products/components/index.ts
+++ src/features/products/components/index.ts
@@ -4,7 +4,7 @@ export * from "./ProductGrid";
 export * from "./ProductGallery";
 export * from "./ProductInfo";
 export * from "./PriceBox";
-
+export * from "./AddToCartButton";
 export * from "./ProductSpecs";
 export * from "./ProductDescription";
 export * from "./RelatedProducts";
