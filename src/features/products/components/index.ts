e8881a77ded8a5cc2d459f9b1fa75356f499700e
tranhuyland <huy07kt@gmail.com>
2026-07-01 07:50:28 +0700

    Update index.ts

--- src/features/products/components/index.ts
+++ src/features/products/components/index.ts
@@ -1,3 +1,10 @@
 export * from "./ProductCard";
 export * from "./ProductGrid";
+
+export * from "./ProductGallery";
+export * from "./ProductInfo";
+export * from "./PriceBox";
+export * from "./AddToCartButton";
+export * from "./ProductSpecs";
+export * from "./ProductDescription";
 export * from "./RelatedProducts";
