61c472588b0681e22327ea3f51bcac1f59db9a96
tranhuyland <huy07kt@gmail.com>
2026-07-01 08:34:26 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { Hero } from "@/components/home/Hero";
+import { Categories } from "@/components/home/Categories";
+import { FeaturedProducts } from "@/components/home/FeaturedProducts";
+import { WhyChooseUs } from "@/components/home/WhyChooseUs";
+import { ZaloCTA } from "@/components/home/ZaloCTA";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
       <Categories />
+      
     </>
   );
 }
