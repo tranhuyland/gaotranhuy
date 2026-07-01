2736faa9cc8ecfbdada878d7a126a024935c8143
tranhuyland <huy07kt@gmail.com>
2026-07-01 18:58:15 +0700

    Update createOrder.ts

--- src/features/checkout/services/createOrder.ts
+++ src/features/checkout/services/createOrder.ts
@@ -1,9 +1,27 @@
-import type { Order } from "../types/order";
+import type { CheckoutFormData } from "../validation";
 
-export async function createOrder(
-  order: Order
-) {
-  console.log("ORDER", order);
+export interface OrderItem {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+interface CreateOrderParams {
+  customer: CheckoutFormData;
+  items: OrderItem[];
+}
+
+export async function createOrder({
+  customer,
+  items,
+}: CreateOrderParams) {
+  // TODO:
+  // Sau này gọi API:
+  // return await api.post("/orders", { customer, items });
+
+  console.log("Customer:", customer);
+  console.log("Items:", items);
 
   await new Promise((resolve) =>
     setTimeout(resolve, 1000)
